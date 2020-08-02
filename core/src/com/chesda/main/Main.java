0499ed5118db8d055fd412fe57b233af7f9a6bc8
rethc <reth@xtra.co.nz>
2020-08-02 21:57:04 +1200

    commit

--- core/src/com/chesda/main/Main.java
+++ core/src/com/chesda/main/Main.java
@@ -3,34 +3,24 @@ package com.chesda.main;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Main extends ApplicationAdapter {
 	ShapeRenderer shape;
-	int x = 50;
-	int y = 50;
-	int xSpeed = 5;
+	Ball ball;
 	
 	@Override
 	public void create () {
 		shape = new ShapeRenderer();
+		ball = new Ball(150, 200, 70, 12, 5);
 	}
 
 	@Override
 	public void render () {
-		x += xSpeed;
-		if (x > Gdx.graphics.getWidth()) {
-			xSpeed = -5;
-		}
-		if (x < 0) {
-			xSpeed = 5;
-		}
-
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		ball.update();
 		shape.begin(ShapeRenderer.ShapeType.Filled);
-		shape.circle(x, y, 50);
+		ball.draw(shape);
 		shape.end();
 	}
 	
