7cef60be66d8d79ee542172ae8cf778b69ca8de2
rethc <reth@xtra.co.nz>
2020-08-02 22:03:02 +1200

    commit

--- core/src/com/chesda/main/Main.java
+++ core/src/com/chesda/main/Main.java
@@ -4,23 +4,36 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.ArrayList;
+import java.util.Random;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
 	ShapeRenderer shape;
-	Ball ball;
+	ArrayList<Ball> balls = new ArrayList<>();
+	Random r = new Random();
 	
 	@Override
 	public void create () {
 		shape = new ShapeRenderer();
-		ball = new Ball(150, 200, 70, 12, 5);
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
+					r.nextInt(Gdx.graphics.getHeight()),
+					r.nextInt(100), r.nextInt(15), r.nextInt(15)));
+		}
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		ball.update();
 		shape.begin(ShapeRenderer.ShapeType.Filled);
-		ball.draw(shape);
+		for (Ball ball : balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+
 		shape.end();
 	}
 	
